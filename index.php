f1d323ab4778ce4726f82ef10c1b7d3737391a75
taransandhu <32505031+taransandhu@users.noreply.github.com>
2017-10-04 11:02:44 +0530

    first
    first

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>First herokuapp</title>
+
+	<style type="text/css">
+		
+		.design{
+      
+			 margin: 100px;
+			width: 1200px;
+			height: 50px;
+			background-color: #00e500;
+
+              color: white;
+		}
+
+
+	</style>
+</head>
+<body>
+
+<div class="design">
+	
+	<center>
+		<h1>Advance Application</h1>
+	</center>
+
+</div>
+
+
+
+
+</body>
+</html>
